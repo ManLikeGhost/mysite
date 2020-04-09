de4047cf907eadb638615074736d59be0ca3ba4a
ManLikeGhost <ekpoabasiama@gmail.com>
2020-04-09 01:17:43 +0100

    Updates

---
+++ precache-manifest.c8022c599bd7f3710eb898ef089d6a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9a1ff193118ea51afb72841ca78fa4",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "2b635105a0bd4ef23a61",
+    "url": "/mysite/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "cc3932e6aeb606b6f64b",
+    "url": "/mysite/static/css/main.ab29ff67.chunk.css"
+  },
+  {
+    "revision": "2b635105a0bd4ef23a61",
+    "url": "/mysite/static/js/2.0ee35257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysite/static/js/2.0ee35257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3932e6aeb606b6f64b",
+    "url": "/mysite/static/js/main.ff3384b5.chunk.js"
+  },
+  {
+    "revision": "fa8faa503ac95ee20c48",
+    "url": "/mysite/static/js/runtime-main.c7545994.js"
+  }
+]);
